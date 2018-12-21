ac9a85dafaa263f713b54d3f0a8c31d5e6ccbb14
maugham <somersetmaughaminjapan@gmail.com>
2018-12-21 11:14:21 +0000

    kadai-microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,11 @@ class User < ApplicationRecord
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
 
+  #お気に入り機能
+  has_many :favorites
+  has_many :favorite_items, through: :favorites, source: :micropost
+  
+  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -44,4 +49,18 @@ class User < ApplicationRecord
     Micropost.where(user_id: self.following_ids + [self.id])
   end
   
+  
+  #お気に入り機能
+  def favorite(favorite_item)
+    self.favorites.find_or_create_by(micropost_id: favorite_item.id)
+  end
+  
+  def unfavorite(favorite_item)
+    favorite = self.favorites.find_by(micropost_id: favorite_item.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorite?(favorite_item)
+    self.favorite_items.include?(favorite_item)
+  end  
 end
